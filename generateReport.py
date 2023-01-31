bc5fa807bf4f114b6a1eb7dbc68cb1a3959456f1
rtz <keoshvi@pm.me>
2023-01-31 11:50:42 -0500

    update

--- generateReport.py
+++ generateReport.py
@@ -74,8 +74,8 @@ if __name__ == "__main__":
         cmd = f"python3 -m semgrep --config={args.directoryRules} {args.directoryRepos}/{fullNameRepo}"
         output = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         stdout,stderr = output.communicate()
-        print(str(stdout,'utf-8'))
-        print(str(stderr,'utf-8'))
+        # print(str(stdout,'utf-8'))
+        # print(str(stderr,'utf-8'))
         with open(f"{args.semgrepResults}_{fullNameRepo}.txt","a") as file:
             file.write(str(stdout,'utf-8'))
 
@@ -91,6 +91,6 @@ if __name__ == "__main__":
         cmd = f"rm -rf {args.directoryRepos}/{fullNameRepo}"
         subprocess.run(cmd,shell=True)
 
-        print(f"{URL},Java,{str(data['stargazers_count'])},{str(data['size'])},{str(findingCount)}\n")
+        #print(f"{URL},Java,{str(data['stargazers_count'])},{str(data['size'])},{str(findingCount)}\n")
         sleep(120)
         
