3749d2a844c7dd1ae7bb790fccbf1d1163289027
rtz <keoshvi@pm.me>
2023-04-07 05:47:11 -0400

    update

--- remove_unwanted_rules.py
+++ remove_unwanted_rules.py
@@ -13,5 +13,5 @@ for file_name in file_names:
     with open(file_name, "r") as somefile:
         file_contents = somefile.read()
         #if "xss" not in file_contents.lower():
-        if not any (string in file_contents.lower() for string in ["xss","csrf","redirect","category: correctness","category: best-practice","this rule has been deprecated","improper encoding or escaping of output","insecure algorithm","cryptographic failures"]):
+        if not any (string in file_contents.lower() for string in ["xss","csrf","redirect","category: correctness","category: best-practice","this rule has been deprecated","improper encoding or escaping of output"]):
             print(file_name)
