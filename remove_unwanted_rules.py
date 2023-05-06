eb2c0482acc6b4934778859ba743fcaac2ae7e12
rtz <keoshvi@pm.me>
2023-05-06 07:57:19 -0400

    updated rules and remove_unwated_rules.py

--- remove_unwanted_rules.py
+++ remove_unwanted_rules.py
@@ -1,17 +1,23 @@
-#Removing unwanted rules (xss,csrf,broken access control etc)
+#Removing unwanted rules (xss,csrf, tls etc) and the ones that have low impact AND low likelihood
 import sys
-viable = []
+import re
 
 with open(sys.argv[1],"r")as file_list:
     file_names = file_list.readlines()
 
 file_names = [file_name.strip() for file_name in file_names]
 
+blacklist_keywords = [
+    "xss","csrf","redirect","category: correctness","category: best-practice",
+    "this rule has been deprecated","improper encoding or escaping of output",
+    "cryptographic failures","improper certificate valbrokation","insecure-resteasy-deserialization",
+    "incorrect type conversion or cast","inadequate encryption strength"]
 
 for file_name in file_names: 
     
     with open(file_name, "r") as somefile:
         file_contents = somefile.read()
         #if "xss" not in file_contents.lower():
-        if not any (string in file_contents.lower() for string in ["xss","csrf","redirect","category: correctness","category: best-practice","this rule has been deprecated","improper encoding or escaping of output", "Cryptographic Failures","Improper Certificate Validation","insecure-resteasy-deserialization"]):
-            print(file_name)
+        if not any (string in file_contents.lower() for string in blacklist_keywords):
+            if not (re.search(r'likelihood: LOW', file_contents) and re.search(r'impact: LOW', file_contents)):
+                print(file_name)
